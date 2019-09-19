dfe54f01c9e93fcc7274f225c3ab4b3039126897
Adrian Gutierrez Alonso <ingenieria.aga@gmail.com>
2019-09-19 14:52:52 +0200

    fix typo in RobotsController

--- app/controllers/api/v1/robots_controller.rb
+++ app/controllers/api/v1/robots_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RobotsController < Api::V1::BaseController
   
   #GET /api/v1/robots  #unauthenticated
   def index
-    @restaurants = policy_scope(Restaurant)
+    @robots = policy_scope(Robot)
   end
-  
+
 end
