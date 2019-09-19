1418f350fcd56ce66afafdcdac9a14e97696c394
Adrian Gutierrez Alonso <ingenieria.aga@gmail.com>
2019-09-19 13:15:11 +0200

    add armor validations

--- app/models/armor.rb
+++ app/models/armor.rb
@@ -1,3 +1,11 @@
 class Armor < ApplicationRecord
+  
+  #Associations
   belongs_to :robot
+
+  #Validations
+  validates :type, presence: true, inclusion: { in: [shield, magnetic field, invisible field],
+    message: "%{value} is not a valid type" }
+  validates :serial_number, format: { with: /^A+\d{6}$/, 
+    message: "Please enter keywords in correct format (letter (A) + 6 digits)"}
 end
