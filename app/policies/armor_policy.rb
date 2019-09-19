2344022cb48ecf8044af6692c9f14956b2dab969
Adrian Gutierrez Alonso <ingenieria.aga@gmail.com>
2019-09-19 23:20:01 +0200

    add armor policy and routes to ArmorsController

---
+++ app/policies/armor_policy.rb
@@ -0,0 +1,26 @@
+class ArmorPolicy < ApplicationPolicy
+    def resolve
+      scope.all
+    end 
+  end
+
+  #Everyone can see the armors.
+  def show?
+    true
+  end
+
+  def update?
+    #Only user owner can update the armors records.
+    record.robot.user == user
+  end
+
+  def create?
+    #Only user owner can create the armors records.
+    update?
+  end
+
+  def destroy?
+    #Only user owner can destroy the armors records as #update
+    update?
+  end
+end
