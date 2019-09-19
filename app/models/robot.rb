67ddb7a04d96ab50ff4f27e23c377083cbbcc719
Adrian Gutierrez Alonso <ingenieria.aga@gmail.com>
2019-09-19 15:34:09 +0200

    fix validation for robot_type

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -7,7 +7,7 @@ class Robot < ApplicationRecord
 
   #Validations
   validates :name, presence: true, uniqueness: true
-  validates :type, presence: true, inclusion: { in: %w(hackerobot protectobot tacticrobot),
+  validates :robot_type, presence: true, inclusion: { in: %w(hackerobot protectobot tacticrobot),
     message: "%{value} is not a valid type" }
   validates :serial_number, format: { with: /\AR+\d{6}\z/, 
     message: "Please enter keywords in correct format (letter (R) + 6 digits)"}
