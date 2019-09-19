9c63bee6f29a7e8a00ff7b13f0888cf542c7ff2d
Adrian Gutierrez Alonso <ingenieria.aga@gmail.com>
2019-09-19 12:44:32 +0200

    add name validation

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,6 +1,10 @@
 class Robot < ApplicationRecord
+  
   #Associations
   belongs_to :user
   has_many :weapons
   has_many :armors
+
+  #Validations
+  validates :name, presence: true, uniqueness: true
 end
