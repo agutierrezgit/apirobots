da4a1903d09441faa900b9d646d893f417aa2e0f
Adrian Gutierrez Alonso <ingenieria.aga@gmail.com>
2019-09-19 16:00:53 +0200

    add show method do RobotsController

--- app/controllers/api/v1/robots_controller.rb
+++ app/controllers/api/v1/robots_controller.rb
@@ -1,8 +1,21 @@
 class Api::V1::RobotsController < Api::V1::BaseController
+    before_action: :set_robot, only: [:show]
   
   #GET /api/v1/robots  #unauthenticated
   def index
     @robots = policy_scope(Robot)
+  end
+
+  #GET /api/v1/robots/:id
+  def show
+    authorize @robot
+
+  end
+
+  private
+
+  def set_robot
+    @robot = Robot.find(params[:id])
   end
 
 end
