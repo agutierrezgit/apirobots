d43563f3b12dbba69c6bc503895e2bbc65476d05
Adrian Gutierrez Alonso <ingenieria.aga@gmail.com>
2019-09-19 13:12:56 +0200

    add weapon validations

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,3 +1,11 @@
 class Weapon < ApplicationRecord
+  
+  #Associations
   belongs_to :robot
+
+  #Validations
+  validates :type, presence: true, inclusion: { in: %w(laser code acid map),
+    message: "%{value} is not a valid type" }
+  validates :serial_number, format: { with: /^W+\d{6}$/, 
+    message: "Please enter keywords in correct format (letter (W) + 6 digits)"}
 end
