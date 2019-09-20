c01089c9535bf7c535b1decf02a2c442db2cd81c
Adrian Gutierrez Alonso <ingenieria.aga@gmail.com>
2019-09-20 10:01:08 +0200

    override controller for UserSession

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
           
     namespace :api, defaults: { format: :json } do
     namespace :v1 do
